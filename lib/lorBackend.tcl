e9762f86026d2daf6adeaa706047f5f047b6dcec
gaa <gaa@b7c15d52-df26-4c05-99ec-4fa05bb6444e>
2009-07-03 16:29:59 +0000

    added LOR backend
    git-svn-id: file:///home/al/Projects/tkLOR-new@156 b7c15d52-df26-4c05-99ec-4fa05bb6444e

---
+++ lib/lorBackend.tcl
@@ -0,0 +1,121 @@
+#!/bin/sh
+############################################################################
+#    Copyright (C) 2008 by Alexander Galanin                               #
+#    gaa.nnov@mail.ru                                                      #
+#                                                                          #
+#    This program is free software; you can redistribute it and/or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 3 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               #
+############################################################################
+
+# \
+exec wish "$0" "$@"
+
+package require Tcl 8.4
+package require cmdline 1.2.5
+
+set appName tkLOR
+
+set configDir [ file join $::env(HOME) ".$appName" ]
+
+if {[ string first Windows $tcl_platform(os) ] == -1} {
+    set libDir "/usr/lib/tkLOR"
+} else {
+    set libDir "."
+}
+
+############################################################################
+#                                 VARIABLES                                #
+############################################################################
+
+set useProxy 0
+set proxyAutoSelect 0
+set proxyHost ""
+set proxyPort ""
+set proxyAuthorization 0
+set proxyUser ""
+set proxyPassword ""
+
+############################################################################
+#                                 FUNCTIONS                                #
+############################################################################
+
+proc loadConfigFile {fileName} {
+    global appName
+
+    if { ![ file exists $fileName ] } {
+        return
+    }
+    catch {
+        set f [ open $fileName "r" ]
+        fconfigure $f -encoding utf-8
+        set data [ read $f ]
+        close $f
+
+        uplevel #0 $data
+    }
+}
+
+proc loadConfig {} {
+    global configDir
+
+    loadConfigFile [ file join $configDir "config" ]
+    loadConfigFile [ file join $configDir "userConfig" ]
+}
+
+proc loadAppLibs {} {
+    global libDir
+    global auto_path
+
+    lappend auto_path $libDir
+
+    package require gaa_lambda 1.0
+    package require lorParser 1.0
+    package require gaa_httpTools 1.0
+
+    namespace import ::gaa::lambda::*
+}
+
+############################################################################
+#                                   MAIN                                   #
+############################################################################
+
+array set param [ ::cmdline::getoptions argv [ list \
+    [ list configDir.arg  $configDir    "Config directory" ] \
+    [ list libDir.arg     $libDir       "Library path" ] \
+    [ list appId.arg      $appName      "Application ID" ] \
+] ]
+
+if { $param(configDir) != "" } {
+    set configDir $param(configDir)
+}
+if { $param(libDir) != "" } {
+    set libDir $param(libDir)
+}
+
+loadConfig
+loadAppLibs
+
+gaa::httpTools::init \
+    -useragent      $param(appId) \
+    -proxy          $useProxy \
+    -autoproxy      $proxyAutoSelect \
+    -proxyhost      $proxyHost \
+    -proxyport      $proxyPort \
+    -proxyauth      $proxyAuthorization \
+    -proxyuser      $proxyUser \
+    -proxypassword  $proxyPassword \
+    -charset        "utf-8"
+
+eval [ read stdin ]
