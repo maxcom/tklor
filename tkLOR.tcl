5316fc938583c54a695705552b3678762b9e58e0
gaa <gaa@b7c15d52-df26-4c05-99ec-4fa05bb6444e>
2009-07-03 16:29:08 +0000

    improved test widget appearance, updated license header
    git-svn-id: file:///home/al/Projects/tkLOR-new@106 b7c15d52-df26-4c05-99ec-4fa05bb6444e

--- tkLOR.tcl
+++ tkLOR.tcl
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License     #
 #    along with this program; if not, write to the                         #
 #    Free Software Foundation, Inc.,                                       #
-#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+#    51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               #
 ############################################################################
 
 # \
@@ -495,7 +495,7 @@ proc renderHtmlTag {w stack tag slash param text} {
                 $w insert end "\n"
             }
             p {
-                if { $text != "" && [ $w get 0.0 end ] != "\n" } {
+                if { [ $w get 0.0 end ] != "\n" } {
                     $w insert end "\n\n"
                 }
                 $stack push [ list $tag $pos ]
